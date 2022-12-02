0ca64bd4a36a4b6e6445ba22502fe3b47ba3738d
Adam <74552077+adam-tracy@users.noreply.github.com>
2022-12-02 12:26:14 +0000

    Update index.py

--- Week01/index.py
+++ Week01/index.py
@@ -5,7 +5,7 @@ elf_fruit = elf_input.split("\n\n")
 elf_sums = []
 for fruit_calories in elf_fruit:
     elf_fruit = [int(item) for item in fruit_calories.split("\n")]
-    elf_sum = sum(fruit_calories)
+    elf_sum = sum(elf_fruit)
     elf_sums.append(elf_sum)
 
 elf_sums.sort()
