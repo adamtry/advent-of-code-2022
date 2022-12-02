2f3b9c28661dd2c18208fae77b7ce36f34ef3aa9
Adam <74552077+adam-tracy@users.noreply.github.com>
2022-12-02 12:35:07 +0000

    Update index.py

--- Week01/index.py
+++ Week01/index.py
@@ -1,12 +1,20 @@
+class Elf():
+    def __init__(self, fruit_calories_list):
+        self.fruit_calories_list = fruit_calories_list
+    
+    def get_total_calories(self):
+        return sum(self.fruit_calories_list)
+
 with open("aoc_input.txt", "r") as outfile:
     elf_input=outfile.read()
-elf_fruit = elf_input.split("\n\n")
+elf_fruit_list = elf_input.split("\n\n")
 
 elf_sums = []
-for fruit_calories in elf_fruit:
-    elf_fruit = [int(item) for item in fruit_calories.split("\n")]
-    elf_sum = sum(elf_fruit)
-    elf_sums.append(elf_sum)
+for fruit_calories in elf_fruit_list:
+    elf = Elf(
+        fruit_calories_list=[int(item) for item in fruit_calories.split("\n")]
+        )
+    elf_sums.append(elf.get_total_calories())
 
 elf_sums.sort()
 
